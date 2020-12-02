dac6cae22b8c48d7bbd3f7e63f1bf707f564560e
vladimir <lovkiy2012@gmail.com>
2020-12-02 19:54:52 +0300

    33 add Image background

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Text, Image } from 'react-native';
 import { AddTodo } from '../components/AddTodo';
 import { Todo } from '../components/Todo';
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+  let content = (
+    <FlatList
+      keyExtractor={(item) => item.id.toString()}
+      data={todos}
+      renderItem={({ item }) => <Todo todo={item} onOpen={openTodo} onRemove={removeTodo} />}
+    />
+  );
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image style={styles.image} source={require('../../assets/no-items.png')} />
+      </View>
+    );
+  }
   return (
     <View>
       <AddTodo onSubmit={addTodo} />
-      <FlatList
-        keyExtractor={(item) => item.id.toString()}
-        data={todos}
-        renderItem={({ item }) => <Todo todo={item} onOpen={openTodo} onRemove={removeTodo} />}
-      />
+      {content}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgWrap: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    height: 300,
+  },
+  image: {
+    resizeMode: 'contain',
+    height: '100%',
+    width: '100%',
+  },
+});
