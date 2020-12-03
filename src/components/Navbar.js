e3548da00d2104cea249b2b05b3d5c3f2e047006
vladimir <lovkiy2012@gmail.com>
2020-12-03 19:47:22 +0300

    40 platform definition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import { THEME } from '../../theme';
 import { AppTextBold } from './UI/AppTextBold';
 
 export const Navbar = ({ title }) => {
   return (
-    <View style={styles.navbar}>
+    <View
+      style={{
+        ...styles.navbar,
+        ...Platform.select({
+          ios: styles.navbarIos,
+          android: styles.navbarAndroid,
+        }),
+      }}
+    >
       <AppTextBold style={styles.text}>{title}</AppTextBold>
     </View>
   );
@@ -16,8 +24,15 @@ const styles = StyleSheet.create({
     height: 80,
     alignItems: 'center',
     justifyContent: 'flex-end',
-    backgroundColor: THEME.MAIN_COLOR,
+
     paddingBottom: 10,
+  },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1,
   },
   text: {
     fontSize: 20,
